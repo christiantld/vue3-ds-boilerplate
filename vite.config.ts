45f6458cb2aec1834e6f1db0ffa885e2823c313e
Christian Toledo <chrstiantoledo@gmail.com>
2022-08-23 19:12:43 -0300

    chore: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
@@ -5,4 +7,8 @@ import eslintPlugin from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
 })
