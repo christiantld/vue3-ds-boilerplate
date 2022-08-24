7b9d8e01401393f5e9d7c122d93edb446317ccf4
Tiago Boer Casagrande <tiago.boer@hotmail.com>
2022-08-24 09:33:55 -0300

    Feature/build library (#1)
    * fix: eslint with prettier bug
    
    * style: format with prettier
    
    * feat: export as library
    
    * chore: add vitest
    
    * chore: add testing library
    
    * chore: add vueuse
    
    Co-authored-by: Christian Toledo <chrstiantoledo@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,17 @@ import eslintPlugin from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
+  build: {
+    minify: false,
+    lib: {
+      formats: ['es'],
+      entry: 'src/main.ts',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: /^vue/,
+    },
+  },
   test: {
     globals: true,
     environment: 'happy-dom',
